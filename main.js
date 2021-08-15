c41723e45e3b67b77f4ff1c02557cb92d9a3eb3c
Hemanth1599 <78473917+Hemanth1599@users.noreply.github.com>
2021-08-15 13:15:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+status = "";
+
+function setup(){
+    canvas = createCanvas(640,500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function start(){
+    objectDetector = ml5.objectDetector('cocossd' , modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Objects Detecting";
+    object_name = document.getElementById("object_type").value;
+}
+
+function modelLoaded(){
+    console.log("modelLoaded!");
+    status = true;
+}
+
+function draw(){
+    image(video , 0 , 0 , 640 , 500);
+}
